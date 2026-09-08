04b6f7fe81e291fbdcbf5b411c30ab221f6620b1
BSM1973 <didguitare@me.com>
2026-09-08 16:07:06 +0200

    feat: add audio settings window to main component

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <juce_audio_utils/juce_audio_utils.h>
 #include <juce_gui_basics/juce_gui_basics.h>
 #include "AudioEngine.h"
 
@@ -15,12 +16,16 @@ public:
     void mouseDown(const juce::MouseEvent& event) override;
 
 private:
+    class AudioSettingsWindow;
+
     void timerCallback() override;
     void drawTransport(juce::Graphics& g, juce::Rectangle<int> area);
     void drawTrackArea(juce::Graphics& g, juce::Rectangle<int> area);
     void drawMixer(juce::Graphics& g, juce::Rectangle<int> area);
+    void openAudioSettings();
 
     AudioEngine audioEngine;
+    std::unique_ptr<AudioSettingsWindow> audioSettingsWindow;
     bool isPlaying = false;
     double playheadSeconds = 0.0;
 
