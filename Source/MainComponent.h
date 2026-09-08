80ae9bf8cad3b5b52c91860a607cc9c367cdd345
BSM1973 <didguitare@me.com>
2026-09-08 18:17:46 +0200

    fix: add clip drag release and track clear control

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -17,6 +17,7 @@ public:
     void resized() override;
     void mouseDown(const juce::MouseEvent& event) override;
     void mouseDrag(const juce::MouseEvent& event) override;
+    void mouseUp(const juce::MouseEvent& event) override;
 
 private:
     class AudioSettingsWindow;
