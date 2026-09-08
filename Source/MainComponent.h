75bab3d1657faf85aad679ae1d867970774354d4
BSM1973 <didguitare@me.com>
2026-09-08 15:02:36 +0200

    feat: build DAW interface foundation

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -2,7 +2,8 @@
 
 #include <juce_gui_basics/juce_gui_basics.h>
 
-class MainComponent final : public juce::Component
+class MainComponent final : public juce::Component,
+                            private juce::Timer
 {
 public:
     MainComponent();
@@ -10,4 +11,15 @@ public:
 
     void paint(juce::Graphics& g) override;
     void resized() override;
+
+private:
+    void timerCallback() override;
+    void drawTransport(juce::Graphics& g, juce::Rectangle<int> area);
+    void drawTrackArea(juce::Graphics& g, juce::Rectangle<int> area);
+    void drawMixer(juce::Graphics& g, juce::Rectangle<int> area);
+
+    bool isPlaying = false;
+    double playheadSeconds = 0.0;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
