df44df151e65ab515d976ec072fc9feaaec728ee
BSM1973 <didguitare@me.com>
2026-09-09 11:53:32 +0200

    fix: add safe MIDI editor shutdown

--- Source/MidiEditor.h
+++ Source/MidiEditor.h
@@ -2,6 +2,5 @@
 
 class MainComponent;
 
-// Opens the MIDI editor for Liberty's first MIDI track.
-// The editor owns no project state; it edits MainComponent::midiEngine directly.
 void openLibertyMidiEditor(MainComponent& owner);
+void shutdownLibertyMidiEditor();
